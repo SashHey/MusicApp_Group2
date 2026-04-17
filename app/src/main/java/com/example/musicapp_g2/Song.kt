b9e463a5a1cae063b4f638eba3b9d2b0e74cc66e
Sasha T Heyburgh <sasha.heyburgh@gmail.com>
2026-04-17 16:17:42 +0200

    Added and initialized constructor

--- app/src/main/java/com/example/musicapp_g2/Song.kt
+++ app/src/main/java/com/example/musicapp_g2/Song.kt
@@ -2,13 +2,21 @@ package com.example.musicapp_g2
 
 import android.util.Log
 
-class Song {
+class Song (songName: String, songArtist: String, songYear: Int, songDuration: String){
 
     //adding variables for song object's attributes
     var name : String = ""
     var artist : String = ""
     var year : Int = 0
     var duration : String = ""
+
+    //initialising the variables/attributes for the song objects
+    init{
+        name = songName
+        artist = songArtist
+        year = songYear
+        duration = songDuration
+    }
 
     //function to play a song
     fun Play(){
